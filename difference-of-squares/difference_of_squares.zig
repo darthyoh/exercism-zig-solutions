97e59224d4d04ce95ae876245794ae212871e7dd
Yohan DUFAUD <ydufaud@sm-adn.fr>
2023-11-21 14:15:05 +0100

    add collatz_conjoncture

--- difference-of-squares/difference_of_squares.zig
+++ difference-of-squares/difference_of_squares.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 
 pub fn squareOfSum(number: usize) usize {
     var sum: usize = 0;
-    var i: usize = 0;
-    while (i <= number) : (i += 1) {
+
+    for (0..number + 1) |i| {
         sum += i;
     }
     return std.math.pow(usize, sum, 2);
@@ -11,8 +11,7 @@ pub fn squareOfSum(number: usize) usize {
 
 pub fn sumOfSquares(number: usize) usize {
     var sum: usize = 0;
-    var i: usize = 0;
-    while (i <= number) : (i += 1) {
+    for (0..number + 1) |i| {
         sum += std.math.pow(usize, i, 2);
     }
     return sum;
